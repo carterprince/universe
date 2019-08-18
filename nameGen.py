3c1fde18fd4817ec1e5d285fdb57e0e67e9861bb
SoopaKhell <carteraprince@gmail.com>
2019-08-17 20:11:08 -0400

    Make aliases/name generators easier to add to things.json

--- nameGen.py
+++ nameGen.py
@@ -20,31 +20,3 @@ def modernName():
     namesJSON = open("data/modernNames",'r').read()
     names = literal_eval(namesJSON)
     return choice(names)
-
-associations = {
-    "medieval-wet-planet": "wet planet with life",
-    "medieval-continent": "inhabited continent",
-    "medieval-head": "head",
-    "medieval-attire": "clothing",
-    "medieval-building": "building",
-    "medieval-civilization": "civilization",
-    "medieval-capital": "capital city",
-    "medieval-town": "town",
-    "medieval-city": "city",
-    "medieval-government-building": "government building",
-    "medieval-house": "house",
-    "medieval-farm": "farm",
-    "medieval-person": medievalName,
-    "medieval-nation": medievalNationName,
-    "medieval-thought": medievalThought
-}
-
-#Main function:
-def getName(thingType):
-    try:
-        if type(associations[thingType]) is str:
-            return associations[thingType]
-        else:
-            return associations[thingType]()
-    except KeyError:
-        return thingType
--- thing.py
+++ thing.py
@@ -1,25 +1,39 @@
 from ast import literal_eval
-thingsJSON = open("things.json",'r').read()
-thingsDict = literal_eval(thingsJSON)
-
+import inspect
 import random
 import nameGen
 
-class Thing:
-    def __init__(self, type):
-        self.Contains = []
-        self.Type = type
+thingsJSON = open("things.json",'r').read()
+thingsDict = literal_eval(thingsJSON)
 
-        self.Name = nameGen.getName(type)
+#every function in nameGen
+nameGenFunctions = [x[0] for x in inspect.getmembers(nameGen, inspect.isfunction) if x[0] != "literal_eval" and x[0] != "getName"]
+
+class Thing:
+    def __init__(self, thingType):
+        self.Contains = []
+        self.Type = thingType
+
+        if self.Type in thingsDict: #if we have created a thing for it yet
+            if type(thingsDict[self.Type][0]) is str: #if it has an alias/name gen
+                if thingsDict[self.Type][0] in nameGenFunctions: #if the alias has a function in the nameGen module
+                    self.Name = eval("nameGen."+thingsDict[self.Type][0]+"()") #run the function, set the name to the output
+                else:
+                    self.Name = thingsDict[self.Type][0] #regular alias, just rename it to what's in things.json
+            else: # if it does not have an alias or name generator, just use the thing's type
+                self.Name = thingType
+        else:
+            self.Name = thingType
     def generateChildren(self):
-        if self.Contains == [] or self.Name == "universe":
+        if self.Contains == [] or self.Name == "universe": # only generate if there's nothing in the object
             try:
-                for potentialChild in thingsDict[self.Type]:
-                    for i in range(potentialChild[1]):
-                        if random.random() < potentialChild[2]:
-                            self.Contains.append(Thing(potentialChild[0]))
+                for potentialChild in thingsDict[self.Type]: #for every contituient of a thing
+                    if type(potentialChild) is tuple: #if the element is not for naming purposes (is an actual thing)
+                        for i in range(potentialChild[1]): # run potentialChild[1] times
+                            if random.random() < potentialChild[2]:
+                                self.Contains.append(Thing(potentialChild[0])) #add the thing to its parent
             except KeyError:
-                self.Contains.append(Thing("WORK IN PROGRESS"))
+                self.Contains.append(Thing("wip"))
     def view(self):
         print("\n- "+self.Name)
         for i, child in enumerate(self.Contains):
